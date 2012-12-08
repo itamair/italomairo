00990bc0e196f03fd4182f6332b4878de8f338b6
Italo Mairo <itamair@mac.com>
2012-12-08 17:11:06 +0100

    commit

--- sites/all/themes/realestate/templates/node--contentvuoto.tpl.php
+++ sites/all/themes/realestate/templates/node--contentvuoto.tpl.php
@@ -1,23 +1,18 @@
 <?php
-//dpm($page);
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
 
   <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
   <?php print render($title_suffix); ?>
 
   <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
-    </div>
+    <span class="submitted"><?php print $submitted ?></span>
   <?php endif; ?>
 
-  <div class="content"<?php print $content_attributes; ?>>
+  <div class="content clearfix"<?php print $content_attributes; ?>>
     <?php
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
@@ -26,9 +21,5 @@
     ?>
   </div>
 
-  <?php //print render($content['links']); ?>
-
-  <?php print render($content['comments']); ?>
 
 </div>
-
