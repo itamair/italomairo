604eb67836f6084b5d1df1cda97ccf717bb78ffe
Italo Mairo <itamair@mac.com>
2012-12-11 22:20:17 +0100

    commit prima di modifiche HP come da richieste FMM

--- sites/all/themes/realestate/ds_layouts/notizia_anteprima/notizia_anteprima.tpl.php
+++ sites/all/themes/realestate/ds_layouts/notizia_anteprima/notizia_anteprima.tpl.php
@@ -24,38 +24,31 @@
   <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
   
-      <?php if ($display_submitted): ?>
-      <div class="submitted">
-          <span class="date"><?php print $date; ?></span>
-          <?php print $name; ?>
-      </div>
-    <?php endif; ?>   
+  <?php if ($image && $image!= '&nbsp;'):  ?>
+    <div class="group-image<?php print $image_classes; ?>">
+      <?php print $image; ?>
+    </div>
+  <?php endif; ?> 
 
-  <?php if ($header): ?>
+  <?php if ($header && $header!= '&nbsp;'): ?>
     <div class="group-header<?php print $header_classes; ?>">
       <?php print $header; ?>
     </div>
   <?php endif; ?>
 
-  <?php if ($suffix): ?>
+  <?php if ($suffix && $suffix!= '&nbsp;'): ?>
     <div class="group-suffix<?php print $suffix_classes; ?>">
       <?php print $suffix; ?>
     </div>
   <?php endif; ?>
 
-  <?php if ($left): ?>
-    <div class="group-left<?php print $left_classes; ?>">
-      <?php print $left; ?>
+  <?php if ($description && $description!= '&nbsp;'): ?>
+    <div class="group-description<?php print $description_classes; ?>">
+      <?php print $description; ?>
     </div>
   <?php endif; ?>
 
-  <?php if ($right): ?>
-    <div class="group-right<?php print $right_classes; ?>">
-      <?php print $right; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($footer): ?>
+  <?php if ($footer && $footer!= '&nbsp;'): ?>
     <div class="group-footer<?php print $footer_classes; ?>">
       <?php print $footer; ?>
     </div>
