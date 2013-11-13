b6853b5b49d50845e1d46c6e5d4a299458c42125
Italo Mairo <itamair@mac.com>
2013-11-13 11:19:20 +0100

    commit

--- sites/all/modules/custom_modules/tc_geofield_gmap/js/geofield_gmap.js
+++
@@ -1,3 +0,0 @@
-//This file should stay empty, as it
-//Ovverides the default js file loaded by the "geofield_gmap" module in its info file,
-//as our map js will be loaded in the tc_geofield_gmap_widget_gmap_afterbuild function
